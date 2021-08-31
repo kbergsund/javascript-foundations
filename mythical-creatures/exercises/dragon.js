8df63cacd2c54a579cb87e8ccf077d15952a344a
Kyra Bergsund <k.bergsund@gmail.com>
2021-08-30 17:44:23 -0700

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
 
+class Dragon {
+  constructor(name, rider) {
+    this.ate = 0;
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+
+  eat() {
+    this.ate += 1;
+    if (this.ate >= 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
