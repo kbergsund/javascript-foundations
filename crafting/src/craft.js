1f3d1a042f880b7e42c8f6c28de7c99e9bafb6ff
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-08 08:02:43 -0700

    Complete crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
 class Craft {
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
+
+  calculateProjectTotal() {
+    var materialCost = 0; 
+    for (var i = 0; i < this.materials.length; i++) {
+      materialCost += this.materials[i].price * this.materials[i].amount;
+    }
+    return materialCost;
+  }
 }
 
 module.exports = Craft;
