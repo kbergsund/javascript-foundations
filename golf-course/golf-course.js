5933fb3579306e051a94ee3a47dbdf88bd582aa9
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-10 16:56:57 -0700

    Complete golf-course

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,23 @@
 class GolfCourse {
+  constructor(name, difficulty, openings, features) {
+    this.name = name;
+    this.difficulty = difficulty;
+    this.openings = openings;
+    this.currentlyPlaying = [];
+    this.features = features;
+  }
 
-
+  checkInGroup(group) {
+    if (group.length < this.openings) {
+      for (var i = 0; i < group.length; i++) {
+        this.currentlyPlaying.unshift(group[i].name);
+        this.openings -= 1;
+      }
+      return 'You\'re checked in. Have fun!';
+    } else {
+      return 'Sorry, we are currently booked! Please come back later.';
+    }
+  }
 }
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,46 @@
 class Golfer {
+  constructor(golfer) {
+    this.name = golfer.name;
+    this.handicap = golfer.handicap;
+    this.frustration = 0;
+    this.confidence = 0;
+  }
 
+  calculateAvg(par) {
+    var average = this.handicap + par;
+    return `I usually shoot a ${average} on average.`;
+  }
 
+  playRound(golfCourse) {
+    if (golfCourse.difficulty === 'hard') {
+      this.frustration += 500;
+    } else {
+      this.frustration += 100;
+    }
+  }
+
+  hitTheRange() {
+    this.confidence += 10;
+  }
+
+  marvel(golfCourse) {
+    var feature1 = golfCourse.features[0];
+    var feature2 = golfCourse.features[1];
+    return `I love the ${feature1} and ${feature2} on this course!`;
+  }
+
+  whatYaShoot(score) {
+    if (!score) {
+      this.frustration -= 10;
+      return 'Booyah!';
+    } else if (score > 0) {
+      this.frustration += 20;
+      return 'Doh!';
+    } else {
+      this.frustration = 0;
+      return 'I AM THE GREATEST GOLFER ALIVE!';
+    }
+  }
 }
 
 module.exports = Golfer;
