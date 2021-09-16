083e20d4146c233bb37c4ac8180c749fca9db918
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-16 16:43:25 -0700

    Complete lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,30 @@
 class LunchBox {
+  constructor(lunchbox) {
+    this.owner = lunchbox.owner;
+    this.material = lunchbox.madeOf;
+    this.shape = lunchbox.shape;
+    this.color = lunchbox.color;
+    this.snacks = [];
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    // loop through snacks array
+    // use checkForHealthy to evaluate true/false
+    // if true, save to new array
+    // at end, return that new array
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type)
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
