41124a2b5c3a1d2837b46c7f0715a5f01f3161d3
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-21 16:25:23 -0700

    Complete trick or treat

---
+++ trick-or-treat/lib/trick-or-treater.js
@@ -0,0 +1,22 @@
+class TrickOrTreater {
+  constructor(costume, bag) {
+    this.dressedUpAs = costume.style;
+    this.bag = bag;
+    this.hasCandy = false;
+    this.countCandies = 0;
+  }
+
+  putCandyInBag(candy) {
+    this.hasCandy = true;
+    this.bag.fill(candy);
+    this.countCandies = this.bag.count;
+  }
+
+
+  eat() {
+    this.countCandies -= 1;
+    this.bag.candies.pop();
+  }
+}
+
+module.exports = TrickOrTreater;
