35435fba134ee21dd6799b48b5f95955c6c8e8cb
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-22 16:58:28 -0700

    Complete fairy

--- mythical-creatures/exercises/fairy.js
+++ mythical-creatures/exercises/fairy.js
@@ -1,5 +1,41 @@
 class Fairy {
+  constructor(name) {
+    this.name = name;
+    this.dust = 10;
+    this.clothes = {
+      dresses: ['Iris']
+    }
+    this.disposition = 'Good natured';
+    this.humanWards = [];
+  }
 
+  receiveBelief() {
+    this.dust += 1;
+  }
+
+  believe() {
+    this.dust += 10;
+  }
+
+  makeDresses(flowers) {
+    for (var i = 0; i < flowers.length; i++) {
+      this.clothes.dresses.push(flowers[i]);
+    }
+  }
+
+  becomeProvoked() {
+    this.disposition = 'Vengeful';
+  }
+
+  replaceInfant(infant) {
+    if (this.humanWards.length < 2) {
+      this.humanWards.push(infant);
+      infant.disposition = 'Malicious';
+      return infant;
+    } else {
+      this.disposition = 'Good natured';
+    }
+  }
 }
 
 module.exports = Fairy;
